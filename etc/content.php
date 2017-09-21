1ea4fecec36b98096f97d980af25d07e76be15ee
Babichev Maxim <info@babichev.net>
2017-09-21 11:52:47 +0300

    change password ;)

--- etc/content.php
+++ etc/content.php
@@ -2,9 +2,9 @@
 
 return [
     'cookies' => [
-        'password' => 'KpFD8L5pgWnItN26r6udkTpFPfjbRClSbvoY3ECa3dmMPcei0HjKeVw0fYxy2CQi'
+        'password' => '%cookies.password%'
     ],
     'session' => [
-        'password' => 'ELCO3XAXCgrpj5DQ6fOh6U0goQjnBy6qU7SlkVZINtd3BA5tuouLwapCr2tiMjc7'
+        'password' => '%session.password%'
     ],
 ];
